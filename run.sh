a50704aae7ff3586a0d6c0c6444461c6909c87a6
Armin <git@armin-warda.de>
2017-10-10 11:34:28 +0200

    added comment wrt. GF_INSTALL_PLUGINS

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash -x
+
 source /etc/sysconfig/grafana-server
+
+# set env variable GF_INSTALL_PLUGINS in deployment config,
+# eg. to "fetzerch-sunandmoon-datasource,hawkular-datasource"
+
 if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
   OLDIFS=$IFS
   IFS=','
   for plugin in ${GF_INSTALL_PLUGINS}; do
-#   grafana-cli --pluginsDir /usr/share/grafana/data/plugins plugins install ${plugin}
-    grafana-cli --pluginsDir ${PLUGINS_DIR}                  plugins install ${plugin}
+    grafana-cli --pluginsDir ${PLUGINS_DIR} plugins install ${plugin}
   done
   IFS=$OLDIFS
 fi
+
 cd /usr/share/grafana
+
 /usr/sbin/grafana-server \
 --config=${CONF_FILE} \
 --pidfile=${PID_FILE} \
